63f5887e87db7904b45060b88867869f3b546b6f
Ciaran <ciarankamp@gmail.com>
2019-03-21 18:00:39 +0000

    adds largest prime factor ts

---
+++ 3_largest_prime_factor.ts
@@ -0,0 +1,61 @@
+// let final: number = 13195;
+let final: number = 503195;
+// const final = 15;
+// let final: number = 600851475143;
+// let final: number = 5415143;
+
+console.time("getPrimes");
+const primesArray = getPrimeNumbers(final, 2);
+console.timeEnd("getPrimes");
+console.time("getFactors");
+const primeFactors = primeFactorFilter(primesArray, final);
+console.timeEnd("getFactors");
+console.time("getLargestFactor");
+const largestPrimeFactor = getLargestPrimeFactor(primeFactors);
+console.timeEnd("getLargestFactor");
+
+// console.info(primesArray);
+console.info(primeFactors);
+// console.log(largestPrimeFactor);
+
+function getPrimeNumbers(target : number, factors : number = 2) {
+  let currentNumber: number = 2;
+  let primes = [];
+  if (currentNumber > 3) {
+    primes = [2, 3];
+    currentNumber = 4;
+  }
+  while (currentNumber <= target / factors) {
+    //  workout if number is prime
+    let x = 2;
+    let isPrime = true;
+    while (x <= currentNumber / 2) {
+      if (isPrime) {
+        if (currentNumber % x == 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      x++;
+    }
+    if (isPrime) {
+      primes.push(currentNumber);
+    }
+    currentNumber++;
+  }
+  return primes;
+}
+
+function primeFactorFilter(array, num : number) {
+  let factors = [];
+  array.forEach(element => {
+    if (num % element == 0) {
+      factors.push(element);
+    }
+  });
+  return factors;
+}
+
+function getLargestPrimeFactor(array) {
+  return Math.max(...array);
+}
